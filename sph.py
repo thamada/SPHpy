fa32caf37dff26f6114047b260de9fd9971fff01
Tsuyoshi Hamada <hamada@progrape.jp>
2016-12-24 01:45:54 +0900

    check python modules

--- sph.py
+++ sph.py
@@ -1,8 +1,14 @@
 #!/usr/bin/env python2
 import OpenGL 
 OpenGL.ERROR_ON_COPYs = True 
+from OpenGL.GL import *
+from OpenGL.GLU import *
+from OpenGL.GLUT import *
+from OpenGL.constants import GLfloat
+import sys, time, math, random
 
-print "Hello PyOpenGL"
+
+print "Hello SPH.py"
 
 
 
