815986b96c86f4a1d17c0634a000a2f822a6ce51
Tsuyoshi Hamada <hamada@progrape.jp>
2016-12-24 02:16:22 +0900

    My life didn't please me, so I created my life.-- Coco Chanel

--- sph.py
+++ sph.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python2
-#Time-stamp: <2016-12-24 02:12:50 hamada>
+#Time-stamp: <2016-12-24 02:14:47 hamada>
 import OpenGL 
 OpenGL.ERROR_ON_COPYs = True 
 from OpenGL.GL import *
@@ -8,6 +8,15 @@ from OpenGL.GLUT import *
 from OpenGL.constants import GLfloat
 import sys, time, math, random
 
+def init():
+    glLightfv(GL_LIGHT0, GL_AMBIENT, vec4(0.5, 0.1,  -0.1, 0.1))
+    glLightfv(GL_LIGHT1, GL_AMBIENT, vec4(0.5, 0.1,  1.0,  0.1))
+    glEnable(GL_CULL_FACE)
+    glEnable(GL_LIGHTING)
+    glEnable(GL_LIGHT0)
+    glEnable(GL_LIGHT1)
+    glEnable(GL_DEPTH_TEST)
+    glEnable(GL_NORMALIZE)
 
 if __name__ == '__main__':
     glutInit(sys.argv)
@@ -15,8 +24,10 @@ if __name__ == '__main__':
     glutInitWindowPosition(800, 0)
     glutInitWindowSize(800, 800)
     glutCreateWindow("SPHpy")
+    init()
 
-    glutMainLoop()
+
+    print "hello"
 
 
 
